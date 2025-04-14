aa0b2fe5fc31486aaa3a74672a17943b70bb0740
Raevhaal <prestegaard9@gmail.com>
2025-04-14 21:16:44 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/FrameData/',
+  base: '/FrameData',
   plugins: [
     react({
       babel: {
